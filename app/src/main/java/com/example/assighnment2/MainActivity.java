9ac3cb2044845d696113520b0cd3a7b85e26dbbc
samarit329 <437202284@student.ksu.edu.sa>
2021-02-10 05:36:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/assighnment2/MainActivity.java
@@ -0,0 +1,46 @@
+/*
+package com.example.assighnment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+*/
+package com.example.assighnment2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    Button mButton;
+    EditText mEdit;
+    TextView mText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm);
+        mButton = (Button)findViewById(R.id.button1);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mEdit   = (EditText)findViewById(R.id.editText1);
+                mText = (TextView)findViewById(R.id.textView1);
+                mText.setText("Welcome "+mEdit.getText().toString()+"!");
+            }
+        });
+    }
+}
